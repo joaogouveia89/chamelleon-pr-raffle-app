0f66e9161b90dd9fa9fc194c5b6cf5c4c9e8dbc6
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2019-11-13 10:55:52 +0000

    resolve migration problems for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_004512) do
+ActiveRecord::Schema.define(version: 1) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2019_11_09_004512) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar"
   end
 
   add_foreign_key "raffles", "users"
